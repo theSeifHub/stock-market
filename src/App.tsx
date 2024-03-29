6a907322e9abd32545d04adcc3a704db8712bbde
Seif Eldeen Ahmed <seifahmed870@yahoo.com>
2024-03-29 04:54:08 +0200

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="">
+      <h1 className="font-mono font-bold text-3xl text-center pt-16">Stock Market</h1>
+    </div>
+  );
+}
+
+export default App;
